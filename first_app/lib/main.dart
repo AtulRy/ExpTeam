2a3220d5846a19bde90c3a2db60afaf3d88fe643
AtulRy <atulreny911@gmail.com>
2023-11-27 23:39:41 +0530

    email+google  authentication(initializing application)

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'pages/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -9,11 +15,10 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return  MaterialApp(
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:LoginPage(),
-      );
+      home: LoginPage(),
+    );
   }
 }
-
