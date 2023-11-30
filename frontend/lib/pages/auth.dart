918b5c645957fec5a23c901a54db73fd468b8a9d
AtulRy <atulreny911@gmail.com>
2023-11-30 16:37:47 +0530

    email auth done

---
+++ frontend/lib/pages/auth.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:first_app/pages/home.dart';
+import 'package:first_app/pages/login_page.dart';
+import 'package:flutter/material.dart';
+
+class auth extends StatelessWidget {
+  const auth({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return HomePage();
+            } 
+            
+            else {
+              return LoginPage();
+            }
+          }),
+    );
+  }
+}
