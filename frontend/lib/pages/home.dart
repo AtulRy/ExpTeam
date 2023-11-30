aea800bf42f029a95e14ecbdb5d304558a00d4aa
AtulRy <atulreny911@gmail.com>
2023-11-30 23:57:24 +0530

    email auth complete progress indicator added for correct login_page error messages not yet added

--- frontend/lib/pages/home.dart
+++ frontend/lib/pages/home.dart
@@ -1,11 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+   HomePage({super.key});
+
+  final user = FirebaseAuth.instance.currentUser!;
+
+  logout() {
+    FirebaseAuth.instance.signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Ur in Bish!Congrats"),),
+      appBar: AppBar(
+        actions: [IconButton(onPressed: logout, icon: Icon(Icons.logout))],
+      ),
+      body: Center(
+        child: Text("Ur in Bish!Congrats "+user.email!,
+        style: TextStyle(fontSize: 20),),
+      ),
     );
   }
 }
